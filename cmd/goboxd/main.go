17a88beb26350661516f075ea18205e3ab6f7419
googleboy <mdasgupta0112@gmail.com>
2026-05-20 16:48:49 +0530

    feat: implement initial HTTP server with health check endpoint

---
+++ cmd/goboxd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.Int("port", 8080, "port to listen on")
+	flag.Parse()
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
+	addr := fmt.Sprintf(":%d", *port)
+	log.Printf("Starting server on %s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("Could not start server: %s", err)
+	}
+}
